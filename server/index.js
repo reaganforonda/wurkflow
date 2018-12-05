2f2c731aaff8353b862e9a6ef5d43690782164a9
reaganforonda <foronda.reagan@gmail.com>
2018-12-04 19:57:43 -0700

    massive

--- server/index.js
+++ server/index.js
@@ -3,13 +3,18 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 const dotenv = require('dotenv');
+const massive = require('massive');
 dotenv.config();
 
-const {SERVER_PORT} = process.env;
+const {SERVER_PORT, CONNECTION_STRING} = process.env;
 
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static(`${__dirname}/../build`));
+
+massive(CONNECTION_STRING).then(dbInstance => {
+    app.set("db", dbInstance);
+  }).catch(e => console.log(`Error: ${e}`));
 
 app.listen(SERVER_PORT, ()=> {
     console.log(`Creeping on port: ${SERVER_PORT}`);
