4486d5f9e371c1b05df3c9404cd436162f0deff0
reaganforonda <foronda.reagan@gmail.com>
2018-12-04 20:57:45 -0700

    login

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 export default class Landing extends React.Component{
 
@@ -9,10 +10,21 @@ export default class Landing extends React.Component{
             pw: ''
         }
         this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleFormSubmit = this.handleFormSubmit.bind(this);
     }
 
     handleInputChange(e) {
         this.setState({[e.target.name]: e.target.value})
+    }
+
+    handleFormSubmit(e) {
+        e.preventDefault();
+        let user = {
+            email: this.state.email,
+            pw : this.state.pw
+        }
+
+        
     }
 
     render(){
@@ -23,11 +35,13 @@ export default class Landing extends React.Component{
                 </header>
                 <main className='landing-main'>
                     <div className='main-center'>
-                        Wurkflow
+                        <h1>Wurkflow</h1>
                     </div>
                     <form className='landing-form'>
-                        <input required={true} type='email' onChange={(e)=>this.handleInputChange(e)} name='email' placeholder='Email' value={this.state.email}/>
-                        <input required={true} type='password' onChange={(e)=>this.handleInputChange(e)} name='pw' placeholder='Password' value={this.state.pw}/>
+                        <input required={true} type='email' onChange={(e)=>this.handleInputChange(e)} 
+                            name='email' placeholder='Email' value={this.state.email}/>
+                        <input required={true} type='password' onChange={(e)=>this.handleInputChange(e)} 
+                            name='pw' placeholder='Password' value={this.state.pw}/>
                         <button>Login</button>
                     </form>
                 </main>
