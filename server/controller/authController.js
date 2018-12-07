3f355747e8663171f4040e2541e36e94acb39369
reaganforonda <foronda.reagan@gmail.com>
2018-12-06 19:45:52 -0700

    register

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcryptjs');
 const genUtil = require('../../src/utilities/genUtil');
+const axios = require('axios');
 
 module.exports = {
     login: (req, res)=> {
@@ -7,8 +8,35 @@ module.exports = {
         const {email, pw} = req.body;
     },
 
-    register: (req, res)=> {
+    register: async (req, res)=> {
         const db = req.app.get('db');
-        const{email, pw} = req.body;
+        const{email, pw, confirmPW, firstName, lastName} = req.body;
+        
+        let lowerCaseEmail = email.toLowerCase();
+
+        if(!genUtil.verifyPW(pw, confirmPW)) {
+            res.sendStatus(400);
+        } else {
+            await db.CHECK_EMAIL([lowerCaseEmail]).then((users)=> {
+                if(users.length !==0) {
+                    if(users[0].email === lowerCaseEmail){
+                        res.status(400).send("Please Login");
+                    }
+                } else {
+                    const salt = bcrypt.genSaltSync(10);
+                    const hash  = bcrypt.hashSync(confirmPW, salt);
+
+                    db.CREATE_USER([lowerCaseEmail, firstName, lastName, hash]).then((result)=> {
+                        res.sendStatus(200);
+                    }).catch((err) => {
+                        console.log(`Server error while attempting to create user: ${err}`)
+                        res.sendStatus(500);
+                    })
+                }
+            }).catch((err) => {
+                console.log(`Server error while attempting to check email at register: ${err}`)
+                res.sendStatus(500);
+            })
+        }
     }
 }
